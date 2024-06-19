da4e8d83b618db4e530ada093ddfb2e63ad7e67c
VlO-OlV <linichenko.vanya@gmail.com>
2024-06-19 17:34:37 +0300

    Add project files.

---
+++ Library/Library.cs
@@ -0,0 +1,106 @@
+﻿namespace SinglyLinkedList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Next { get; set; }
+        
+        public Node(int data)
+        {
+            Value = data;
+            Next = null;
+        }
+    }
+    public class LinkedList
+    {
+        public Node? Head { get; private set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public LinkedList(int headValue)
+        {
+            Head = new Node(headValue);
+        }
+
+        public void add(int nodeValue)
+        {
+            if (Head == null)
+            {
+                Head = new Node(nodeValue);
+            } else
+            {
+                Node newNode = new Node(nodeValue);
+                newNode.Next = Head.Next;
+                Head.Next = newNode;
+            }
+        }
+
+        public int findNode(int nodeValue)
+        {
+            int index = -1;
+            Node? currentNode = Head;
+            while (currentNode != null && currentNode.Value <= nodeValue)
+            {
+                currentNode = currentNode.Next;
+                index = (currentNode == null) ? -1 : index + 1;
+            }
+            return index + 1;
+        }
+
+        public int sum(int nodeValue)
+        {
+            int sum = 0;
+            Node? currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value < nodeValue)
+                {
+                    sum += currentNode.Value;
+                }
+                currentNode = currentNode.Next;
+            }
+            return sum;
+        }
+
+        public LinkedList getGreaterValues(int nodeValue)
+        {
+            LinkedList list = new LinkedList();
+            Node? currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value > nodeValue)
+                {
+                    list.add(currentNode.Value);
+                }
+                currentNode = currentNode.Next;
+            }
+            return list;
+        }
+
+        public void removeAfterMax()
+        {
+            Node? currentNode = Head;
+            if (currentNode != null)
+            {
+                int maxValue = currentNode.Value;
+                while (currentNode != null)
+                {
+                    if (maxValue < currentNode.Value)
+                    {
+                        maxValue = currentNode.Value;
+                    }
+                    currentNode = currentNode.Next;
+                }
+                currentNode = Head;
+                while (currentNode.Value != maxValue)
+                {
+                    currentNode = currentNode.Next;
+                }
+                currentNode.Next = null;
+            }
+        }
+    }
+}
---
+++ Main/Main.cs
@@ -0,0 +1,58 @@
+﻿using SinglyLinkedList;
+
+void printList(LinkedList list)
+{
+    Node? currentNode = list.Head;
+    while (currentNode != null)
+    {
+        Console.Write($"{currentNode.Value} ");
+        currentNode = currentNode.Next;
+    }
+}
+
+LinkedList list = new LinkedList();
+int option = 1;
+
+while (option < 6 && option > 0)
+{
+    Console.Write("List: ");
+    printList(list);
+    Console.WriteLine("\n========================\nChoose an option:\n1) Add node\n2) Find first greater node\n3) Find sum of smaller nodes\n4) Get new list of greater nodes\n5) Remove nodes after max node\n6) Exit\n========================");
+    option = Convert.ToInt32(Console.ReadLine());
+    if (option == 1)
+    {
+        Console.Write("Enter a value of the node: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+        list.add(value);
+    }
+    if (option == 2)
+    {
+        Console.Write("Enter a value: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+        int index = list.findNode(value);
+        Console.WriteLine($"Result: {index}");
+        Console.ReadLine();
+    }
+    if (option == 3)
+    {
+        Console.Write("Enter a value: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+        int sum = list.sum(value);
+        Console.WriteLine($"Result: {sum}");
+        Console.ReadLine();
+    }
+    if (option == 4)
+    {
+        Console.Write("Enter a value: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+        LinkedList resultList = list.getGreaterValues(value);
+        Console.Write("Result: ");
+        printList(resultList);
+        Console.ReadLine();
+    }
+    if (option == 5)
+    {
+        list.removeAfterMax();
+    }
+    Console.Clear();
+}
